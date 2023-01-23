7930a8579be8a9aca7229f26cbe94caf51a3a86e
pm3512 <aobolens@andrew.cmu.edu>
2023-01-23 11:32:32 -0500

    fix linter issues

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,17 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import fibonacci = require("./fib");
 
-export default (req, res) => {
+export default (req: {
+  params: {
+    num: string
+  }
+}, res: {
+  send: (number) => void
+}) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
+  const fibN = (fibonacci as (number) => number)(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
